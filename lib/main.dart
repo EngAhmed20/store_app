cdaab6ec344eb47243a7d66804c6931fceefecdd
EngAhmed20 <ahmed2012muhammad@gmail.com>
2025-05-06 19:11:58 +0300

    env setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:store_app/core/app/env.variables.dart';
 import 'package:store_app/firebase_options.dart';
 import 'package:store_app/store_app.dart';
 
@@ -8,6 +9,7 @@ void main() async{
   await Firebase.initializeApp(
    options: DefaultFirebaseOptions.currentPlatform,
   );
+  await EnvVariable.instance.init(envType: EnvTypeEnum.dev);
   runApp(const StoreApp());
 }
 
--- lib/store_app.dart
+++ lib/store_app.dart
@@ -7,13 +7,14 @@ class StoreApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home:Scaffold(
-        appBar:AppBar(title: Text('Store App'),) ,
+        appBar:AppBar(title: Text('Store App'),
+        centerTitle: true,) ,
 
       ),
     );
