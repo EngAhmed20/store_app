8dee96bb3f3f2c2566ae09eb0bbea37e7130e764
EngAhmed20 <ahmed2012muhammad@gmail.com>
2025-05-22 13:24:29 +0300

    setup shared pref,freezed and bloc

---
+++ lib/core/service/shared_pref/pref_keys.dart
@@ -0,0 +1,7 @@
+class PrefKeys{
+  static const String themeMode = 'theme_mode';
+  static const String languageCode = 'language_code';
+  static const String isLoggedIn = 'is_logged_in';
+  static const String userId = 'user_id';
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,19 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:store_app/core/app/bloc_observer.dart';
 import 'package:store_app/core/app/connectivity_controller.dart';
 import 'package:store_app/core/app/env.variables.dart';
 import 'package:store_app/firebase_options.dart';
 import 'package:store_app/store_app.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
    options: DefaultFirebaseOptions.currentPlatform,
   );
+  Bloc.observer = AppBlocObserver();
   await EnvVariable.instance.init(envType: EnvTypeEnum.dev);
   await ConnectivityController.instance.init();
 
